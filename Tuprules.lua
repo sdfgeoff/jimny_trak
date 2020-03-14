e3e4087c0143f6711113ff337fc9067c5fc2bf1a
Geoffrey Irons <sdfgeoff@gmail.com>
2020-03-14 14:33:02 +1300

    Version1. Unfortunately there isn't enough "wrap" around the drive wheel, so the servo doesn't move the track when it rotates

---
+++ Tuprules.lua
@@ -0,0 +1,8 @@
+TOP = tup.getcwd()
+blender = 'export PYTHONDONTWRITEBYTECODE=1; blender '
+STL_FOLDER = TOP .. '/generated/stl'
+GEN_BLEND_FOLDER = TOP .. '/generated/blends'
+GEN_IMAGE_FOLDER = TOP .. '/generated/images'
+
+PROJECT_NAME = "Traktion_Maschine_3"
+VERSION = "v0.1"
